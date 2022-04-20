fdb324907538a53ffc1568efa1574b0e2715411e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-20 22:39:05 +0600

    Context api done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Component/Home/Home';
+import { UserProvider } from './Component/UserContext/UserContext';
 
 function App() {
   return (
     <div className="App">
+      <UserProvider>
      <BrowserRouter>
      <Routes>
       <Route path='/' element={<Home></Home>}/>
      </Routes>
      </BrowserRouter>
+     </UserProvider>
     </div>
   );
 }
