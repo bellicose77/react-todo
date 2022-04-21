2c04d674e549c038e6d09267c018aee2b653dcae
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-21 22:01:02 +0600

    modal page

--- src/Component/Delete/Delete.js
+++ src/Component/Delete/Delete.js
@@ -1,13 +1,28 @@
 import React, { useContext } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../UserContext/UserContext';
 
 const Delete = () => {
-    const[users,setUsers]=useContext(UserContext)
-    const{id}=useParams()
+    const[users,setUsers]=useContext(UserContext);
+    const{id}=useParams();
+    const user = users.filter(user=>user.id!==id)
     return (
         <div>
-            <h2>This is delete page  {id}</h2>
+            <Modal.Dialog>
+  <Modal.Header closeButton>
+    <Modal.Title>Modal title</Modal.Title>
+  </Modal.Header>
+
+  <Modal.Body>
+    <p>Modal body text goes here.</p>
+  </Modal.Body>
+
+  <Modal.Footer>
+    <Button variant="secondary">Close</Button>
+    <Button variant="primary">Save changes</Button>
+  </Modal.Footer>
+</Modal.Dialog>
         </div>
     );
 };
