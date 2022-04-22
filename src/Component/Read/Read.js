b3ceb495eb928b56bb550827e4254babddfa366c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-22 07:44:13 +0600

    showing data

--- src/Component/Read/Read.js
+++ src/Component/Read/Read.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { UserContext } from '../UserContext/UserContext';
 
 const Read = () => {
-
+    const[users,setUsers]=useContext(UserContext)
     const {id}=useParams();
+    const user = users.filter(user => user.id===id);
+    
     return (
         <div>
             <h2>The information of User:{id} </h2>
+            <p>Name : {user.name}</p>
+            <p>Profession:{user.position}</p>
+            <p>Salary:{user.salary}</p>
         </div>
     );
 };
