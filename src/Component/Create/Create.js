6e53f81d1f7ff998fffa4635a40b6b99838095bb
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-22 08:19:25 +0600

    form created

--- src/Component/Create/Create.js
+++ src/Component/Create/Create.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 const Create = () => {
     return (
         <div>
-            <h2>This is create page</h2>
+         <Form >
+        <Form.Group>
+          <Form.Label>ID</Form.Label>
+          <Form.Control
+            type="text"
+            name="id"
+            value={}
+            onChange={updateId}
+            placeholder="Enter ID"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            value={}
+            onChange={}
+            placeholder="Enter Name"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Position</Form.Label>
+          <Form.Control
+            type="text"
+            name="position"
+            value={}
+            onChange={}
+            placeholder="Enter Position"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Salary</Form.Label>
+          <Form.Control
+            type="text"
+            name="salary"
+            value={}
+            onChange={}
+            placeholder="Enter Salary"
+          />
+        </Form.Group>
+        </Form>
         </div>
     );
 };
