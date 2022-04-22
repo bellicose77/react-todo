8441ed7edab87357993077c4a8b55dcce0bb7b8b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-22 07:48:15 +0600

    read done

--- src/Component/Read/Read.js
+++ src/Component/Read/Read.js
@@ -5,15 +5,15 @@ import { UserContext } from '../UserContext/UserContext';
 const Read = () => {
     const[users,setUsers]=useContext(UserContext)
     const {id}=useParams();
-    const user = users.filter(user => user.id===id);
+    const user = users.filter(user => user.id==id);
     console.log(user);
     
     return (
         <div>
             <h2>The information of User:{id} </h2>
-            <p>Name : {user.name}</p>
-            <p>Profession:{user.position}</p>
-            <p>Salary:{user.salary}</p>
+            <p>Name : {user[0].name}</p>
+            <p>Profession:{user[0].position}</p>
+            <p>Salary:{user[0].salary}</p>
         </div>
     );
 };
