8cb12e7489e7507152c321599ca9db632ac75bc0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-22 07:41:20 +0600

    id

--- src/Component/Read/Read.js
+++ src/Component/Read/Read.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Read = () => {
+
+    const {id}=useParams();
     return (
         <div>
-            <h2>The information of User: </h2>
+            <h2>The information of User:{id} </h2>
         </div>
     );
 };
